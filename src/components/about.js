e736ab53cde24258721e7ffd76170f3ee87194d9
Chase Davis <chasewdavis7@gmail.com>
2017-12-11 04:06:35 -0700

    tweaks

--- src/components/about.js
+++ src/components/about.js
@@ -16,20 +16,9 @@ class About extends Component {
             <h4>Chase Davis</h4>
             </header>
 
-            {/* <div className='top_left'>
-            <h3>Hey, I'm Chase Davis</h3>
-
-            <div className='paragraphs'>
-            <p>A developer with a passion for creating beautiful, full stack web applications.
-            A graduate from <span>Eastern Washington University</span> with a degree in finance.
-            Now pursuing a career web development, with help from <span>DevMountain</span>, Utah's full stack web development bootcamp.</p>
-            <p>I'm constantly working new and exciting projects so feel free to explore some of my <span>latest work</span>.</p>
-            </div>
-
-            </div> */}
-            {/* <div className='top_right'></div>
-            <div className='bottom_left'></div>
-            <div className='bottom_right'></div> */}
+            <footer>
+              <i class="fa fa-angle-down fa-5x" aria-hidden="true"></i>
+            </footer>
 
         </div>
      
